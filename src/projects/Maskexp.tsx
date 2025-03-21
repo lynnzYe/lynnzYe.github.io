2c1e6e5b1d4a03775d95db16e6fa468b9bd7a37e
Linzan <lye11@u.rochester.edu>
2025-03-21 16:09:24 +0800

    update mask paper link

--- src/projects/Maskexp.tsx
+++ src/projects/Maskexp.tsx
@@ -219,21 +219,27 @@ export default function MaskExp() {
 
             {/* Action Buttons */}
             <div className="flex flex-wrap justify-center gap-4">
-              <Button disabled={true}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                Paper coming soon
-              </Button>
+              <a
+                href="https://drpress.org/ojs/index.php/HSET/article/view/28800"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button disabled={false}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Paper
+                </Button>
+              </a>
               <a
                 href="https://github.com/lynnzYe/MaskedExpressiveness"
                 target="_blank"
